fa47cb9c9299b26f7730fb364910abb7f7f0dcbe
Charlely Delgrange <charlely.delgrange@gmail.com>
2023-01-09 18:58:54 +0100

    fix: use fetch API instead of complicated AJAX request

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-(function() {
-  var httpRequest;
+(() => {
+  const makeRequest = async () => {
+    fetch('http://localhost:3000/users')
+      .then((response) => response.json())
+      .then((data) => alert(JSON.stringify(data, null, 2)));
+  }
+
   document.getElementById("ajaxButton").addEventListener('click', makeRequest);
-
-  function makeRequest() {
-    httpRequest = new XMLHttpRequest();
-
-    if (!httpRequest) {
-      alert('Abandon :( Impossible de créer une instance de XMLHttpRequest');
-      return false;
-    }
-    httpRequest.onreadystatechange = alertContents;
-    httpRequest.open('GET', 'http://localhost:3000/users');
-    httpRequest.send();
-  }
-
-  function alertContents() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        alert(httpRequest.responseText);
-      } else {
-        alert('Il y a eu un problème avec la requête.');
-      }
-    }
-  }
 })();
