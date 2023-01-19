4819f0930f8a48dc6c9e62be5ba54e533d55c16c
Charlely Delgrange <charlely.delgrange@gmail.com>
2023-01-19 15:57:14 +0100

    Add new interface for LoL game

--- index.js
+++ index.js
@@ -1,9 +1,67 @@
-(() => {
-  const makeRequest = async () => {
-    fetch('http://localhost:3000/users')
-      .then((response) => response.json())
-      .then((data) => alert(JSON.stringify(data, null, 2)));
+const apiUrl = 'http://localhost:3000';
+const authUrl = `${apiUrl}/auth`;
+const userUrl = `${apiUrl}/users`;
+const lolUrl = `${apiUrl}/lol`;
+
+const lastChampSelected = '';
+
+(async () => {
+  const getJwt = async () => {
+    const tokenObj = await fetch(`${authUrl}/login`, {
+      method: 'POST',
+      body: JSON.stringify({ firstName: 'Charlely', password: 'password' }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((response) => response.json());
+
+    return tokenObj.token;
   }
 
-  document.getElementById("ajaxButton").addEventListener('click', makeRequest);
+  const jwt = await getJwt();
+
+  document.getElementById('get-champ-list').addEventListener('click', async () => {
+    const champNames = await fetch(`${lolUrl}/champion-names`, { headers: { Authorization: `Bearer ${jwt}` }}).then((response) => response.json());
+
+    const ul = document.getElementById('champ-name-list');
+    for (const name of champNames) {
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(name));
+      ul.appendChild(li);
+    }
+
+    document.getElementById('champ-names').style.display = 'block';
+  });
+
+  document.getElementById('champ-name-list').addEventListener('click', async (event) => {
+    const champName = event.target.innerHTML;
+
+    // Image handling
+    const champImgContainer = document.getElementById('champ-img');
+    champImgContainer.innerHTML = '';
+
+    const img = document.createElement('img');
+    img.src = `${apiUrl}/assets/${champName}_0.jpg`;
+    img.onload = () => champImgContainer.append(img)
+    img.onerror = () => {
+      const span = document.createElement('span');
+      span.innerText = 'Failed to load the champ image';
+      champImgContainer.append(span);
+    }
+
+    // Champ lore handling
+    const champLoreDiv = document.getElementById('champ-lore');
+    const champLoreContainer = champLoreDiv.querySelector('span');
+    const champLoreResponse = await fetch(
+      `${lolUrl}/champion-lore/${champName}`,
+      { headers: { Authorization: `Bearer ${jwt}` }}
+    );
+
+    let loreText = 'Lore not found';
+    if (champLoreResponse.status === 200) {
+      const jsonResponse = await champLoreResponse.json();
+      loreText = jsonResponse.champLore;
+    }
+
+    champLoreContainer.innerText = loreText;
+    champLoreDiv.style.display = 'block';
+  })
 })();
